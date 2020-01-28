372f2cc949c0ee17e279f9a09dce47ee3dc5f401
hcalderon73 <calderonhumberto@gmail.com>
2020-01-28 08:51:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -137,7 +137,7 @@ const Swipeout = createReactClass({
     };
   },
 
-  componentWillMount: function () {
+  componentDidMount: function () {
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (event, gestureState) => true,
       onStartShouldSetPanResponderCapture: (event, gestureState) =>
